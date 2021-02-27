2adab5e2eb9c98526ca81bda6953d58eac53620d
SaiMuraliRoyal <79736441+SaiMuraliRoyal@users.noreply.github.com>
2021-02-27 10:11:59 +0530

    Add files via upload

---
+++ VirtualAssistance/VirtualAssistant.py
@@ -0,0 +1,83 @@
+import speech_recognition as sr
+import pyttsx3
+import pywhatkit
+import datetime
+import wikipedia
+import pyjokes
+
+listener = sr.Recognizer()
+engine = pyttsx3.init()
+voices = engine.getProperty('voices')
+engine.setProperty('voice', voices[0].id)
+
+
+
+def talk(text):
+    engine.say(text)
+    engine.runAndWait()
+
+
+def take_command():
+    try:
+        with sr.Microphone() as source:
+            print('listening...')
+            voice = listener.listen(source)
+            command = listener.recognize_google(voice)
+            command = command.lower()
+            if 'dino' in command:
+                command = command.replace('dino', '')
+                print(command)
+    except:
+        pass
+    return command
+
+def run_dino():
+    i=1
+    if (i==1):
+        talk('Welcome to handfree world')
+        talk('I\'m Dyno, what can i do for you???')
+    i+=1
+    command = take_command()
+    print(command)
+    if 'play' in command:
+        song = command.replace('play', '')
+        talk('playing ' + song)
+        pywhatkit.playonyt(song)
+    elif 'time' in command:
+        time = datetime.datetime.now().strftime('%I:%M %p')
+        talk('Current time is ' + time)
+    elif 'wiki' in command:
+        person = command.replace('wiki', '')
+        info = wikipedia.summary(person, 1)
+        print(info)
+        talk(info)
+    elif 'who is' in command:
+        person = command.replace('who is', '')
+        info = wikipedia.summary(person, 1)
+        print(info)
+        talk(info)
+    elif 'what is' in command:
+        person = command.replace('what is', '')
+        info = wikipedia.summary(person, 1)
+        print(info)
+        talk(info)
+    elif 'where is' in command:
+        person = command.replace('where is', '')
+        info = wikipedia.summary(person, 1)
+        print(info)
+        talk(info)
+    elif 'date' in command:
+        talk('sorry, I have a headache')
+    elif 'are you single' in command:
+        talk('I am in a relationship with wifi')
+    elif 'joke' in command:
+        talk(pyjokes.get_joke())
+    elif 'exit' in command:
+        talk('Had been a pleasure to work for you.')
+        return 0;
+    else:
+        talk('Come again')
+
+
+while True:
+    run_dino()
